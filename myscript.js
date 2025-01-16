37537d50d3ef6974cf3a73cc1bd5a3988486c980
keithkattt <keith110606@gmail.com>
2025-01-16 20:58:47 +0800

    Modified variable "choice" to "computerChoice"

--- myscript.js
+++ myscript.js
@@ -1,15 +1,15 @@
 function getComputerChoice(){
     const randomNum = Math.round(Math.random() * 2) + 1;
-    let choice = "";
+    let computerChoice = "";
         switch(randomNum){
             case 1:
-                choice = "Rock";
+                computerChoice = "Rock";
                 break;
             case 2:
-                choice = "Paper";
+                computerChoice = "Paper";
                 break;
             case 3:
-                choice = "Scissors";
+                computerChoice = "Scissors";
                 break;
         }
 
